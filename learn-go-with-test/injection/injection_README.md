4ff74dabfeefc61e2586c29c17540def919ad87d
Awtanabe <panser098@gmail.com>
2025-01-26 23:35:43 +0900

    削除

--- learn-go-with-test/injection/injection_README.md
+++
@@ -1,9 +0,0 @@
-### 例
-
-```go
-func Greet(name string) {
-    fmt.Printf("Hello, %s", name)
-}
-```
-
-どおやってテストする？ fmt.Printfを
--- learn-go-with-test/loop/repeat.go
+++
@@ -1,9 +0,0 @@
-package iteration
-
-func Repeat(s string) string {
-	var res string
-	for i := 0; i < 5; i++ {
-		res += s
-	}
-	return res
-}
